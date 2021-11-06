5f8c358a8d94346b49fce7851d5d4b40011beb8d
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2021-11-06 11:11:56 -0500

    contact us bones

--- src/main/contact/index.tsx
+++ src/main/contact/index.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function Contact() {
-  return <div>Contact Us Anytime</div>;
+  return (
+    <div>
+      <div>
+        <h1>Contact Us Anytime</h1>
+        <p>
+          Thank you so much for reaching out to us regarding an order! We are in
+          Rosharon, Texas, and offer services to the greater Houston for local
+          pick up or delivery. Please contact us with any pricing or cookie
+          designs that are not featured on our website. If you are ready to
+          place an order, please head over to the {<a href="/order-inquiry" >Order Now</a>} page and complete
+          the order form.
+        </p>
+      </div>
+      <div className="contact-us-form-container">
+
+      </div>
+    </div>
+  );
 }
--- src/main/home/index.tsx
+++ src/main/home/index.tsx
@@ -5,18 +5,19 @@ export default function Home() {
   return (
     <div>
       <div>
-      <h1>Welcome</h1>
-      <p>
-        Welcome to Sugar Society Sugar Cookies the newest black owned small
-        business bringing you beautifully designed sugar cookies for your
-        special event. Our cookies can provide that one of a kind touch to any
-        birthday celebration, wedding, baby shower or corporate event. We’re
-        located in Rosharon, TX and providing our amazing cookies and customer
-        service to the greater Houston area.
-      </p>
-      <h2>Got Questions? Contact Us</h2>
+        <h1>Welcome</h1>
         <p>
-          We can be reached anytime via the contact form which will email us immediately.
+          Welcome to Sugar Society Sugar Cookies the newest black owned small
+          business bringing you beautifully designed sugar cookies for your
+          special event. Our cookies can provide that one of a kind touch to any
+          birthday celebration, wedding, baby shower or corporate event. We’re
+          located in Rosharon, TX and providing our amazing cookies and customer
+          service to the greater Houston area.
+        </p>
+        <h2>Got Questions? Contact Us</h2>
+        <p>
+          We can be reached anytime via the contact form which will email us
+          immediately.
         </p>
         <a href="/contact">
           <button>Contact Form</button>
