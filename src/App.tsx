6d13d319203385dd1cc5a72b7f36dd65289ff70f
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2021-11-06 10:08:33 -0500

    more bones

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,101 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./main/home";
+import OurStory from "./main/home";
+import Specials from "./main/home";
+import OurCookies from "./main/home";
+import OrderInquiry from "./main/home";
+import Contact from "./main/home";
 import { FacebookOutlined, InstagramOutlined } from "@ant-design/icons";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="main-nav-container">
-          <nav className="main-nav">
-            <div key="home" className="menu-item">
-              Home
-            </div>
-            <div key="story" className="menu-item">
-              Our Story
-            </div>
-            <div key="specials" className="menu-item">
-              Specials
-            </div>
-            <div key="home" className="menu-item">
-              <img
-                className="nav-logo"
-                src="https://wellcall-app-cdk.s3.amazonaws.com/sugar-society-sugar-cookies.png"
-              />
-            </div>
-            <div key="cookies" className="menu-item">
-              Our Cookies
-            </div>
-            <div key="order" className="menu-item">
-              Order Inquiry
-            </div>
-            <div key="contact" className="menu-item">
-              Contact Us
-            </div>
-          </nav>
-        </div>
-        <img
-          src={
-            "https://wellcall-app-cdk.s3.amazonaws.com/winter-sugarcookies.jpg"
-          }
-          className="App-logo"
-          alt="logo"
-        />
-      </header>
-      <body className="App-body-container">
-        <div className="App-body">
-          <p>Website Coming Soon!!!</p>
-        </div>
-      </body>
-      <footer>
-        <a className="App-link" href="mailto:ashuah.tomlin@sugarsocietysc.com">
-          Contact Us Now
-        </a>
-        <a
-          href="https://www.facebook.com/Sugar-Society-Sugar-Cookies-105693268589749"
-          target="_blank"
-        >
-          <FacebookOutlined />
-        </a>
-        <a href="https://www.instagram.com/sugarsocietysc/" target="_blank">
-          <InstagramOutlined />
-        </a>
-        <a className="App-link" href="tel:+1254-313-3972">
-          Call Us Now
-        </a>
-      </footer>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div className="main-nav-container">
+            <nav className="main-nav">
+              <div key="home" className="menu-item">
+                Home
+              </div>
+              <div key="story" className="menu-item">
+                Our Story
+              </div>
+              <div key="specials" className="menu-item">
+                Specials
+              </div>
+              <div key="home" className="menu-item">
+                <img
+                  className="nav-logo"
+                  src="https://wellcall-app-cdk.s3.amazonaws.com/sugar-society-sugar-cookies.png"
+                />
+              </div>
+              <div key="cookies" className="menu-item">
+                Our Cookies
+              </div>
+              <div key="order" className="menu-item">
+                Order Inquiry
+              </div>
+              <div key="contact" className="menu-item">
+                Contact Us
+              </div>
+            </nav>
+          </div>
+          <img
+            src={
+              "https://wellcall-app-cdk.s3.amazonaws.com/winter-sugarcookies.jpg"
+            }
+            className="App-logo"
+            alt="logo"
+          />
+        </header>
+        <body className="App-body-container">
+          <div className="App-body">
+            <Switch>
+              <Route path="/our-story">
+                <OurStory />
+              </Route>
+              <Route path="/specials">
+                <Specials />
+              </Route>
+              <Route path="/our-cookies">
+                <OurCookies />
+              </Route>
+              <Route path="/order-inquiry">
+                <OrderInquiry />
+              </Route>
+              <Route path="/contact">
+                <Contact />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </body>
+        <footer>
+          <a
+            className="App-link"
+            href="mailto:ashuah.tomlin@sugarsocietysc.com"
+          >
+            Contact Us Now
+          </a>
+          <a
+            href="https://www.facebook.com/Sugar-Society-Sugar-Cookies-105693268589749"
+            target="_blank"
+          >
+            <FacebookOutlined />
+          </a>
+          <a href="https://www.instagram.com/sugarsocietysc/" target="_blank">
+            <InstagramOutlined />
+          </a>
+          <a className="App-link" href="tel:+1254-313-3972">
+            Call Us Now
+          </a>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/main/home/index.tsx
@@ -0,0 +1,11 @@
+import { LoadingOutlined } from "@ant-design/icons";
+import React from "react";
+
+export default function Home() {
+  return <div>
+      <p>
+        Website Currently Baking!!!
+      </p>
+      <LoadingOutlined />
+  </div>;
+}
