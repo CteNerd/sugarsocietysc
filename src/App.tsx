96d619b151f6b6f49ec365b1a33ba291f482ff72
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2021-11-06 08:30:34 -0500

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'https://wellcall-app-cdk.s3.amazonaws.com/sugar-society-sugar-cookies.png'} className="App-logo" alt="logo" />
-        <p>
-          Website Coming Soon!!!
-        </p>
-        <a
-          className="App-link"
-          href="mailto:ashuah.tomlin@sugarsocietysc.com"
-        >
+        <img
+          src={
+            "https://wellcall-app-cdk.s3.amazonaws.com/sugar-society-sugar-cookies.png"
+          }
+          className="App-logo"
+          alt="logo"
+        />
+        <p>Website Coming Soon!!!</p>
+        <a className="App-link" href="mailto:ashuah.tomlin@sugarsocietysc.com">
           Contact Us Now
         </a>
-        <a
-          className="App-link"
-          href="tel:+1254-313-3972"
-        >
+        <a className="App-link" href="tel:+1254-313-3972">
           Call Us Now
         </a>
       </header>
+      <body></body>
+      <footer></footer>
     </div>
   );
 }
