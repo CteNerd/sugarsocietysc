10524dcb2f99feca365d12bd256d29974b60a5bb
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2021-11-06 09:44:54 -0500

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FacebookOutlined, InstagramOutlined } from '@ant-design/icons';
+import { FacebookOutlined, InstagramOutlined } from "@ant-design/icons";
 import "./App.css";
 
 function App() {
@@ -18,7 +18,10 @@ function App() {
               Specials
             </div>
             <div key="home" className="menu-item">
-              <img className="nav-logo" src="https://wellcall-app-cdk.s3.amazonaws.com/sugar-society-sugar-cookies.png" />
+              <img
+                className="nav-logo"
+                src="https://wellcall-app-cdk.s3.amazonaws.com/sugar-society-sugar-cookies.png"
+              />
             </div>
             <div key="cookies" className="menu-item">
               Our Cookies
@@ -41,20 +44,21 @@ function App() {
       </header>
       <body className="App-body-container">
         <div className="App-body">
-        <p>Website Coming Soon!!!</p>
+          <p>Website Coming Soon!!!</p>
         </div>
       </body>
       <footer>
         <a className="App-link" href="mailto:ashuah.tomlin@sugarsocietysc.com">
           Contact Us Now
         </a>
-        <a href="https://www.facebook.com/Sugar-Society-Sugar-Cookies-105693268589749" target="_blank" >
-
-        <FacebookOutlined />
+        <a
+          href="https://www.facebook.com/Sugar-Society-Sugar-Cookies-105693268589749"
+          target="_blank"
+        >
+          <FacebookOutlined />
         </a>
-        <a href="https://www.instagram.com/sugarsocietysc/" target="_blank" >
-
-        <InstagramOutlined />
+        <a href="https://www.instagram.com/sugarsocietysc/" target="_blank">
+          <InstagramOutlined />
         </a>
         <a className="App-link" href="tel:+1254-313-3972">
           Call Us Now
