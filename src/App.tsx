6745a60856b021c048ec81690386a6d15ac5928c
rtomlin62 <roddytdog@yahoo.com>
2021-11-09 13:53:35 -0600

    sidenav implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./main/home";
 import OurStory from "./main/story";
@@ -8,39 +8,70 @@ import OrderInquiry from "./main/order";
 import Contact from "./main/contact";
 import { FacebookOutlined, InstagramOutlined } from "@ant-design/icons";
 import "./App.css";
+import SideNav from "./main/sideNav/sideNav";
 
 function App() {
+  const breakpoint = window.matchMedia('(max-width: 1279px)');
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(breakpoint.matches);
+  }, []);
+
+  /* Set the width of the side navigation to 250px */
+  function openNav() {
+    if (document.getElementById("mySidenav")) {
+      document.getElementById("mySidenav")!.style.width = "250px";
+    }
+  }
+
+  function Navigation() {
+    if (isMobile) {
+      return (
+        <div className="col-33 menu-btn" onClick={() => openNav()}>
+          <div className="menu-line"></div>
+          <div className="menu-line"></div>
+          <div className="menu-line"></div>
+        </div>
+        )
+    } else {
+      return (
+        <nav className="main-nav">
+          <div key="home" className="menu-item">
+            <a href="/">Home</a>
+          </div>
+          <div key="story" className="menu-item">
+            <a href="/our-story">Our Story</a>
+          </div>
+          <div key="specials" className="menu-item">
+            <a href="/specials">Specials</a>
+          </div>
+          <div key="home" className="menu-item">
+            <img
+              className="nav-logo"
+              src="https://wellcall-app-cdk.s3.amazonaws.com/sugar-society-sugar-cookies.png"
+            />
+          </div>
+          <div key="cookies" className="menu-item">
+            <a href="/our-cookies">Our Cookies</a>
+          </div>
+          <div key="order" className="menu-item">
+            <a href="/order-inquiry">Order Inquiry</a>
+          </div>
+          <div key="contact" className="menu-item">
+            <a href="/contact">Contact Us</a>
+          </div>
+        </nav>
+      )
+    }
+  }
   return (
     <Router>
       <div className="App">
+        <SideNav />
         <header className="App-header">
           <div className="main-nav-container">
-            <nav className="main-nav">
-              <div key="home" className="menu-item">
-                <a href="/">Home</a>
-              </div>
-              <div key="story" className="menu-item">
-                <a href="/our-story">Our Story</a>
-              </div>
-              <div key="specials" className="menu-item">
-                <a href="/specials">Specials</a>
-              </div>
-              <div key="home" className="menu-item">
-                <img
-                  className="nav-logo"
-                  src="https://wellcall-app-cdk.s3.amazonaws.com/sugar-society-sugar-cookies.png"
-                />
-              </div>
-              <div key="cookies" className="menu-item">
-                <a href="/our-cookies">Our Cookies</a>
-              </div>
-              <div key="order" className="menu-item">
-                <a href="/order-inquiry">Order Inquiry</a>
-              </div>
-              <div key="contact" className="menu-item">
-                <a href="/contact">Contact Us</a>
-              </div>
-            </nav>
+            <Navigation />
           </div>
         </header>
         <body className="App-body-container">
---
+++ src/main/sideNav/sideNav.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import {
+  HomeOutlined,
+  InfoCircleOutlined,
+  TeamOutlined,
+  MailOutlined,
+  LogoutOutlined,
+  CaretDownOutlined,
+  UserAddOutlined,
+  UsergroupAddOutlined,
+  CalendarOutlined,
+  ScheduleOutlined,
+  CarryOutOutlined,
+  PlusSquareOutlined,
+  BarsOutlined,
+  SafetyOutlined,
+  ShoppingOutlined,
+  UnorderedListOutlined,
+  UserOutlined,
+  BookOutlined,
+  StarOutlined,
+  PieChartOutlined,
+  ProfileOutlined,
+  ShoppingCartOutlined,
+  AlertOutlined,
+} from "@ant-design/icons";
+import "./sideNav.css";
+
+export default function SideNav() {
+
+  const [showEventsDropdown, setShowEventsDropdown] = useState(false);
+  const [showTeamDropdown, setShowTeamDropdown] = useState(false);
+  const [showTicketDropdown, setShowTicketDropdown] = useState(false);
+  const [isTixHolder, setIsTixHolder] = useState(true);
+  const [canAddUsers, setCanAddUsers] = useState(false);
+  const [canAddEvents, setCanAddEvents] = useState(false);
+  const [canViewEventDetails, setCanViewEventDetails] = useState(false);
+  const [isMemberOfOrg, setIsMemberOfOrg] = useState(false);
+
+
+  function closeNav() {
+    if (document.getElementById("mySidenav"))
+      document.getElementById("mySidenav")!.style.width = "0";
+  }
+
+  let content = <></>;
+  content = (
+    <>
+      <a href="/organization-registration">
+        <TeamOutlined /> Add Your Team to We'll Call Tix
+      </a>
+      <a href="/tix-holder-registration">
+        <UserOutlined /> Join We'll Call Tix Yourself
+      </a>
+      <a href="/login">
+        Login
+      </a>
+    </>
+  );
+
+  return (
+    <div id="mySidenav" className="sidenav">
+      <div className="closebtn" onClick={closeNav}>
+        &times;
+      </div>
+      <a href="/">
+        <HomeOutlined /> Home
+      </a>
+      <a href="/our-story">
+        <BookOutlined /> Our Story
+      </a>
+      <a href="/specials">
+        <StarOutlined /> Specials
+      </a>
+      <a href="/our-cookies">
+        <PieChartOutlined /> Our Cookies
+      </a>
+      <a href="/order">
+        <ShoppingCartOutlined /> Order Inquiry
+      </a>
+      <a href="/contact">
+        <AlertOutlined /> Contact Us
+      </a>
+    </div>
+  );
+}
