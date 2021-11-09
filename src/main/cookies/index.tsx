e39d2ee1f31a27974827c5a411d0695f89950bd1
rtomlin62 <roddytdog@yahoo.com>
2021-11-09 15:59:54 -0600

    prepping cookies page

--- src/main/cookies/index.tsx
+++ src/main/cookies/index.tsx
@@ -1,5 +1,49 @@
 import React from "react";
+import "./cookies.css";
 
 export default function OurCookies() {
-  return <div>Mmm Mmm Good... Say Less...</div>;
+  return (
+    <div>
+      <h1>Our Cookies</h1>
+      <h2>
+      Mmm Mmm Good... Say Less...
+      </h2>
+      <div className="row">
+        <div className="column">
+          <div className="card">
+            <a
+              href={
+                "https://wellcall-app-cdk.s3.amazonaws.com/winter-sugarcookies.jpg"
+              }
+            >
+              <img
+                src={
+                  "https://wellcall-app-cdk.s3.amazonaws.com/Tech+Logo+No+Padding.png"
+                }
+                className={"card-img"}
+              />
+              <h3>{"Christmas Cookies"}</h3>
+              <p>{"Just need a glass of milk and a tray just for Santa"}</p>
+            </a>
+          </div>
+          <div className="card">
+            <a
+              href={
+                "https://wellcall-app-cdk.s3.amazonaws.com/winter-sugarcookies.jpg"
+              }
+            >
+              <img
+                src={
+                  "https://wellcall-app-cdk.s3.amazonaws.com/Tech+Logo+No+Padding.png"
+                }
+                className={"card-img"}
+              />
+              <h3>{"Birthday Cookies"}</h3>
+              <p>{"Making life a little sweeter for that special someone in your life"}</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
