93a847deb5aabf86ad7711ce63b47f058a796f5d
rtomlin62 <roddytdog@yahoo.com>
2021-11-09 22:35:20 -0600

    trying to get SendGrid going

--- src/main/order/index.tsx
+++ src/main/order/index.tsx
@@ -1,10 +1,47 @@
 import React from "react";
+import sgMail from '@sendgrid/mail'
 
 export default function OrderInquiry() {
+sgMail.setApiKey('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');
+
+function TrySending() {
+  console.log('we are trying');
+  const msg = {
+    to: 'admin@sugarsocietysc.com',
+    from: 'no-reply@sugarsocietysc.com', // Use the email address or domain you verified above
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  //ES6
+  sgMail
+    .send(msg)
+    .then(() => {}, error => {
+      console.error(error);
+  
+      if (error.response) {
+        console.error(error.response.body)
+      }
+    });
+  //ES8
+  (async () => {
+    try {
+      await sgMail.send(msg);
+    } catch (error: any) {
+      console.error(error);
+  
+      if (error.response) {
+        console.error(error.response.body)
+      }
+    }
+  })();
+  console.log('we tried');
+}
+
   return (
     <div>
       <h1>Order via Email</h1>
-      <div>
+      <div onClick={() => {TrySending()}}>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
