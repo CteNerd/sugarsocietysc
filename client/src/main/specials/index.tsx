822c7a64a1eba81c88231eba18ed675190807271
rtomlin62 <roddytdog@yahoo.com>
2021-11-27 15:49:40 -0600

    add specials img

--- client/src/main/specials/index.tsx
+++ client/src/main/specials/index.tsx
@@ -10,23 +10,10 @@ export default function Specials(props: Props) {
     <div>
       <div>
         <h1>Looking for the specials?</h1>
-        <div className="specials-header">
-          <div className={props.isMobile ? "col-12" : "col-4"}>
-            <img
-              className="specials-img"
-              src="https://wellcall-app-cdk.s3.amazonaws.com/thank-you-scrabble.jpg"
-            />
-          </div>
-          <p className="specials-context">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
+        <img
+          className="specials-img"
+          src="https://wellcall-app-cdk.s3.amazonaws.com/Christmas+Cookie+Sale+Advertisment+Picture.jpg"
+        />
         <a href="/order-inquiry">
           <button>Order Now</button>
         </a>
