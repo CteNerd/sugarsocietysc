89dc12321474020575134892669fe36b271fdcfd
rtomlin62 <roddytdog@yahoo.com>
2021-11-27 15:36:58 -0600

    .

--- client/src/main/story/index.tsx
+++ client/src/main/story/index.tsx
@@ -6,13 +6,7 @@ export default function OurStory() {
       <h1>Once Upon A Time...</h1>
       <div>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Coming Soon...
         </p>
       </div>
     </div>
