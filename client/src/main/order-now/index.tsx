13dc93654fa8036f8c473d1c484663157c839b4e
rtomlin62 <roddytdog@yahoo.com>
2022-01-18 12:06:28 -0600

    order now forms. Prettier Format

--- client/src/main/order-now/index.tsx
+++ client/src/main/order-now/index.tsx
@@ -5,16 +5,21 @@ export default function OrderNow() {
   return (
     <div>
       <div>
-        <h1>Contact Us Anytime</h1>
+        <h1>Order Now</h1>
         <p>
-          Complete Order Form Below.
+          Please complete the short form below and attach a completed order form
+          to your submission. We strive to respond within 24 hours to all order
+          request
         </p>
+      </div>
+      <div>
+        <p>Download Form then attach to Order Submission</p>
+        <a href=""><button>Valentine's Day Order Form</button></a>
+        <a href=""><button>Custom Order Form</button></a>
       </div>
       <div className="contact-us-form-container">
         <a href="https://forms.gle/Rp9JpVfMj3XpV3FN7" target={"_blank"}>
-          <button>
-            Submit Order
-            </button>
+          <button>Submit Order</button>
         </a>
       </div>
     </div>
