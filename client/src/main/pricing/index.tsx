dc834bf419cc820fe55df206e16dbac1413e45d8
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2022-01-18 19:20:29 -0600

    css love. message body for pricing removed

--- client/src/main/pricing/index.tsx
+++ client/src/main/pricing/index.tsx
@@ -8,13 +8,7 @@ export default function Pricing() {
       <h1>Pricing</h1>
       <div>
         <p>
-          Feel free to take a look around our site and get inspiration for your
-          cookie ideas. Once you have decided on what kind of cookie(s) you are
-          looking for, please download and complete one of the order forms
-          below. Completed order forms can be sent via this{" "}
-          {<a href="mailto:ashuah.tomlin@sugarsocietysc.com">link</a>}. Once
-          your order is received, we will contact you to go over details for the
-          order.
+          TBD
         </p>
       </div>
       <div className='pricing-table-container'>
