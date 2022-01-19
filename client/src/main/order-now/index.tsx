c3a4b7b33fb40f6113677c28d9818b277e3fc089
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2022-01-18 20:36:41 -0600

    add order forms

--- client/src/main/order-now/index.tsx
+++ client/src/main/order-now/index.tsx
@@ -14,10 +14,10 @@ export default function OrderNow() {
       </div>
       <div>
         <p>Download Form then attach to Order Submission</p>
-        <a href="">
+        <a href="https://wellcall-app-cdk.s3.amazonaws.com/Valentines+Day.pdf" target={"_blank"} download>
           <button>Valentine's Day Order Form</button>
         </a>
-        <a href="">
+        <a href="https://wellcall-app-cdk.s3.amazonaws.com/Custom+Order+Form.pdf" target={"_blank"} download>
           <button>Custom Order Form</button>
         </a>
       </div>
