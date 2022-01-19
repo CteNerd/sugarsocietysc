b83408060d8385a7e0bf0bd0dc611c917aee5c2b
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2022-01-18 20:37:44 -0600

    specials ad

--- client/src/main/specials/index.tsx
+++ client/src/main/specials/index.tsx
@@ -12,7 +12,7 @@ export default function Specials(props: Props) {
         <h1>Looking for the specials?</h1>
         <img
           className="specials-img"
-          src="https://wellcall-app-cdk.s3.amazonaws.com/Christmas+Cookie+Sale+Advertisment+Picture.jpg"
+          src="https://wellcall-app-cdk.s3.amazonaws.com/Valentines+Pre-Order+Menu.jpg"
         />
         <a href="/order-now">
           <button>Order Now</button>
