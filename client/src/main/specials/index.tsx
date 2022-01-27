8b72a01b9931acdb49ac32a85e897622a6cc4a43
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2022-01-27 13:51:09 -0600

    remove the specials header (pointless)

--- client/src/main/specials/index.tsx
+++ client/src/main/specials/index.tsx
@@ -9,7 +9,6 @@ export default function Specials(props: Props) {
   return (
     <div>
       <div>
-        <h1>Looking for the specials?</h1>
         <img
           className="specials-img"
           src="https://wellcall-app-cdk.s3.amazonaws.com/Valentines+Pre-Order+Menu.jpg"
