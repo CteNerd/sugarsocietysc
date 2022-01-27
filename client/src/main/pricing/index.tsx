3cbba717d5cf3ef1cf4c76c68f5aab5e2feae937
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2022-01-27 15:33:38 -0600

    redoing the look of the pricing page

--- client/src/main/pricing/index.tsx
+++ client/src/main/pricing/index.tsx
@@ -13,25 +13,25 @@ export default function Pricing() {
         <div className="pricing-table">
           <table>
             <tr className="pricing-table-header tooltip">
-              <td>
+              <td className={"pricing-table-item"}>
                 Standard Cookie Order (2 Dozen)
                 <span className="tooltiptext">$2.29/cookie</span>
               </td>
               <td className="pricing-table-price">$55.00</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>Additional Cookies</td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>Additional Cookies</td>
               <td className="pricing-table-price">$2.29</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>
                 Un-Iced Standard Cookie Order
                 <span className="tooltiptext">24 cookies, no icing</span>
               </td>
               <td className="pricing-table-price">$36.00</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>
                 Specialty Cookie Order (1 Dozen)
                 <span className="tooltiptext">$3/cookie</span>
               </td>
@@ -42,17 +42,17 @@ export default function Pricing() {
         <div className="pricing-table">
           <table>
             <tr className="pricing-table-header tooltip">
-              <td>Cookie Design Extras</td>
+              <td className={"pricing-table-item"}>Cookie Design Extras</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>
                 Writing on Cookie
                 <span className="tooltiptext">per cookie</span>
               </td>
               <td className="pricing-table-price">$0.50</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>
                 Cookie Design Difficulty
                 <span className="tooltiptext">
                   based on level of difficulty
@@ -60,43 +60,43 @@ export default function Pricing() {
               </td>
               <td className="pricing-table-price">$0.25 - $1.00</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>
                 Cookie Stencil
                 <span className="tooltiptext">per cookie</span>
               </td>
               <td className="pricing-table-price">$0.50</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>
                 Custom Cookie Stencil
                 <span className="tooltiptext">per stencil added to order</span>
               </td>
               <td className="pricing-table-price">$2.00</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>
                 Additional Colors
                 <span className="tooltiptext">per color added to order</span>
               </td>
               <td className="pricing-table-price">$0.50</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>
                 Character Cookie
                 <span className="tooltiptext">addtional per cookie</span>
               </td>
               <td className="pricing-table-price">$1.00</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>
                 Replicated Cookie
                 <span className="tooltiptext">additional per cookie</span>
               </td>
               <td className="pricing-table-price">$2.71</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>
                 Additional Cookie Cutter Design
                 <span className="tooltiptext">per cookie cutter design</span>
               </td>
@@ -107,14 +107,14 @@ export default function Pricing() {
         <div className="pricing-table">
           <table>
             <tr className="pricing-table-header tooltip">
-              <td>Packaging</td>
+              <td className={"pricing-table-item"}>Packaging</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>Box w/ Zig Zag Paper</td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>Box w/ Zig Zag Paper</td>
               <td className="pricing-table-price">$1.00</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>Bakery Box w/ Zig Zag Paper</td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>Bakery Box w/ Zig Zag Paper</td>
               <td className="pricing-table-price">$1.50</td>
             </tr>
           </table>
@@ -122,7 +122,7 @@ export default function Pricing() {
         <div className="pricing-table">
           <table>
             <tr className="pricing-table-header tooltip">
-              <td>Shipping</td>
+              <td className={"pricing-table-item"}>Shipping</td>
               <td className="pricing-table-price">TBD</td>
             </tr>
           </table>
@@ -130,10 +130,10 @@ export default function Pricing() {
         <div className="pricing-table">
           <table>
             <tr className="pricing-table-header tooltip">
-              <td>Other</td>
+              <td className={"pricing-table-item"}>Other</td>
             </tr>
-            <tr className="pricing-table-item tooltip">
-              <td>
+            <tr className="pricing-table-row tooltip">
+              <td className={"pricing-table-item"}>
                 Rush Order
                 <span className="tooltiptext">36 Hour Notice or less</span>
               </td>
