a0da8924ed649a8b2a549d83643f1e0dcbb0458a
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2022-01-30 11:09:21 -0600

    added v day pix to home page

--- client/src/main/home/index.tsx
+++ client/src/main/home/index.tsx
@@ -53,27 +53,108 @@ export default function Home(props: HomeProps) {
 
       <div className="slideshow-container">
         <div className="mySlides fade">
-          <div className="numbertext">1 / 3</div>
+          <div className="numbertext">1 / 12</div>
           <img
-            src="https://wellcall-app-cdk.s3.amazonaws.com/AmazonPeccy.jpeg"
+            src="https://wellcall-app-cdk.s3.amazonaws.com/ConversationHearts.jpg"
             className="slide-img"
           />
           <div className="text">Caption Text</div>
         </div>
 
         <div className="mySlides fade">
-          <div className="numbertext">2 / 3</div>
+          <div className="numbertext">2 / 12</div>
           <img
-            src="https://wellcall-app-cdk.s3.amazonaws.com/BirthdayBlue.jpeg"
+            src="https://wellcall-app-cdk.s3.amazonaws.com/V-Day-Tic-Tac-Toe.jpg"
             className="slide-img"
           />
           <div className="text">Caption Two</div>
         </div>
 
         <div className="mySlides fade">
-          <div className="numbertext">3 / 3</div>
+          <div className="numbertext">3 / 12</div>
           <img
-            src="https://wellcall-app-cdk.s3.amazonaws.com/ChristmasGroup.jpeg"
+            src="https://wellcall-app-cdk.s3.amazonaws.com/You+Hold+The+Key+To+My+Heart.jpeg"
+            className="slide-img"
+          />
+          <div className="text">Caption Three</div>
+        </div>
+
+        <div className="mySlides fade">
+          <div className="numbertext">4 / 12</div>
+          <img
+            src="https://wellcall-app-cdk.s3.amazonaws.com/Will+You+Be+My+Valentine.jpeg"
+            className="slide-img"
+          />
+          <div className="text">Caption Three</div>
+        </div>
+
+        <div className="mySlides fade">
+          <div className="numbertext">5 / 12</div>
+          <img
+            src="https://wellcall-app-cdk.s3.amazonaws.com/You+Got+My+Spidey+Senses+Tingling.jpg"
+            className="slide-img"
+          />
+          <div className="text">Caption Three</div>
+        </div>
+
+        <div className="mySlides fade">
+          <div className="numbertext">6 / 12</div>
+          <img
+            src="https://wellcall-app-cdk.s3.amazonaws.com/Will+You+Be+My+Valentine+(Pink).jpeg"
+            className="slide-img"
+          />
+          <div className="text">Caption Three</div>
+        </div>
+
+        <div className="mySlides fade">
+          <div className="numbertext">7 / 12</div>
+          <img
+            src="https://wellcall-app-cdk.s3.amazonaws.com/Will+You+Be+My+Valentine+(Red).jpeg"
+            className="slide-img"
+          />
+          <div className="text">Caption Three</div>
+        </div>
+
+        <div className="mySlides fade">
+          <div className="numbertext">8 / 12</div>
+          <img
+            src="https://wellcall-app-cdk.s3.amazonaws.com/V-Day-TicTacToe.jpeg"
+            className="slide-img"
+          />
+          <div className="text">Caption Three</div>
+        </div>
+
+        <div className="mySlides fade">
+          <div className="numbertext">9 / 12</div>
+          <img
+            src="https://wellcall-app-cdk.s3.amazonaws.com/V-Day-TicTacToe-YouWonMyHeart.jpeg"
+            className="slide-img"
+          />
+          <div className="text">Caption Three</div>
+        </div>
+
+        <div className="mySlides fade">
+          <div className="numbertext">10 / 12</div>
+          <img
+            src="https://wellcall-app-cdk.s3.amazonaws.com/Whale+You+Be+My+Valentine.jpg"
+            className="slide-img"
+          />
+          <div className="text">Caption Three</div>
+        </div>
+
+        <div className="mySlides fade">
+          <div className="numbertext">11 / 12</div>
+          <img
+            src="https://wellcall-app-cdk.s3.amazonaws.com/V-Day+XO+XO.jpeg"
+            className="slide-img"
+          />
+          <div className="text">Caption Three</div>
+        </div>
+
+        <div className="mySlides fade">
+          <div className="numbertext">12 / 12</div>
+          <img
+            src="https://wellcall-app-cdk.s3.amazonaws.com/V-Day+TicTacToe+Game.jpeg"
             className="slide-img"
           />
           <div className="text">Caption Three</div>
