3fd156bc7844e7c717405e68954c1413dabd93bd
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2022-01-30 11:34:55 -0600

    remove extra component on home

--- client/src/main/home/index.tsx
+++ client/src/main/home/index.tsx
@@ -160,14 +160,6 @@ export default function Home(props: HomeProps) {
           <div className="text">Caption Three</div>
         </div>
       </div>
-
-      <br />
-
-      <div style={{ textAlign: "center" }}>
-        <span className="dot" onClick={() => currentSlide(1)}></span>
-        <span className="dot" onClick={() => currentSlide(2)}></span>
-        <span className="dot" onClick={() => currentSlide(3)}></span>
-      </div>
     </div>
   );
 }
